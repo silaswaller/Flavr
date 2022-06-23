8629d5da132c54bc1d827e0d9be64fe880c0db23
bo98117 <bosin98117@gmail.com>
2022-06-23 08:52:45 -0700

    Backend Flavr model, controller, routes

---
+++ server/models/flavr.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+
+
+const FlavrSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+    },
+
+    image: {
+        type: String,
+    },
+
+    cookTime: {
+        type: String,
+    },
+
+    serves: {
+        type: String,
+    },
+
+    description: { 
+        type: String,
+    },
+
+    preparation: {
+        type: String,
+    },
+
+    ingredients: {
+        type: [],
+    },
+
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User" 
+    }
+
+}, {timestamps:true})
+
+const Flavr = mongoose.model("Flavr", FlavrSchema);
+
+module.exports = Flavr;
