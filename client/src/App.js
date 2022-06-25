77cff85fcc60af4005e42bd809a9ef95bb48f1a4
Jenalee Palmer <jenaleepalmer@gmail.com>
2022-06-25 01:29:20 -0500

    Added some components and css
    Added a header component for use on all pages except the landing page, login page, and register page. Also added a form to be used with editing or adding a recipe.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+import LoginRegister from './components/LoginRegister';
 import Login from './components/Login';
 import Register from './components/Register';
+import RecipeForm from './components/RecipeForm';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <Home /> } />
+          <Route path='/' element={ <LoginRegister /> } />
           <Route path='/login' element={ <Login /> } />
           <Route path='/register' element={ <Register /> } />
+          <Route path='/home' element={ <Home /> } />
+          <Route path='/new' element={ <RecipeForm /> } />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
+import Header from '../Header';
 import './Home.css';
-import { Link } from 'react-router-dom';
 
 function Home() {
 
     return(
         <div className='homeContainer'>
-            <h1 className='logo'>Flavr</h1>
-            <h2>A Delicious Social Destination</h2>
-            <div className='btnContainer'>
-                <Link to='/login' className='btn'>Login</Link>
-                <Link to='/register' className='btn'>Sign Up</Link>
-            </div>
+            <Header />
         </div>
     );
 };
