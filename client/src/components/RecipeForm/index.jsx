93901203025705d86664dd0cc9dbeb2b906e7fd9
Jenalee Palmer <jenaleepalmer@gmail.com>
2022-06-26 15:00:40 -0500

    Added recipe form component
    Component changes based on the view prop, if it's === 'edit', form values will be autofilled, if not, they remain empty.

--- client/src/components/RecipeForm/index.jsx
+++ client/src/components/RecipeForm/index.jsx
@@ -1,51 +1,160 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './RecipeForm.css';
 import Header from '../Header';
 import addIcon from './plus.png';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
 
-function RecipeForm() {
-    //will add states to each input when I have a model to interact with
-    //will also be adding a conditional for if this form is used for adding a new recipe or editing one
+function RecipeForm(props) {
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
+    const {id} = useParams();
+    const {view} = props;
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+    const [cookTime, setCookTime] = useState('');
+    const [serves, setServes] = useState('');
+    const [description, setDescription] = useState('');
+    const [preparation, setPreparation] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const [createdBy, setCreatedBy] = useState(''); //Not quite sure how to format this based on model
+
+    useEffect(()=>{
+        if(view === 'edit') {
+            axios.get(`http://localhost:8000/api/Recipes/${id}`)
+            .then((res)=>{
+                console.log(res.data);
+                setName(res.data.name);
+                setImage(res.data.image);
+                setCookTime(res.data.cookTime);
+                setServes(res.data.serves);
+                setDescription(res.data.description);
+                setPreparation(res.data.preparation);
+                //setIngredients here somehow
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+        }
+    }, [])
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if(view === 'edit'){
+            axios.put(`http://localhost:8000/api/Recipes/${id}`, {
+                name,
+                image,
+                cookTime,
+                serves,
+                description,
+                preparation
+            })
+                .then( res => {
+                    console.log(res);
+                    navigate('/home');
+                })
+                .catch((err)=>{
+                    const errorResponse = err.response.data.errors;
+                    const errorArr = [];
+                    for (const key of Object.keys(errorResponse)) {
+                        errorArr.push(errorResponse[key].message)
+                    }
+                    setErrors(errorArr);
+                })
+        } else {
+            axios.post('http://localhost:8000/api/Recipes', {
+                name,
+                image,
+                cookTime,
+                serves,
+                description,
+                preparation
+            })
+                .then(res=>{
+                    console.log(res);
+                    console.log(res.data);
+                    navigate('/home');
+                })
+                .catch((err)=>{
+                    const errorResponse = err.response.data.errors;
+                    const errorArr = [];
+                    for (const key of Object.keys(errorResponse)) {
+                        errorArr.push(errorResponse[key].message)
+                    }
+                    setErrors(errorArr);
+                })
+        }
+    }
 
     return(
         <div className='recipeFormContainer'>
             <Header />
-            <h1 className='recipeFormH1'>Add A Recipe!</h1>
-            <form className='recipeForm'>
+            {
+                view === 'edit' ?
+                <h1 className='recipeFormH1'>Edit</h1> :
+                <h1 className='recipeFormH1'>Add A Recipe!</h1>
+            }
+            <form className='recipeForm' onSubmit={submitHandler}>
+                <div className='recipeFormErrorBox'>
+                    {errors.map((err, index) => <p key={index}>{err}</p>)}
+                </div>
                 <div className='formColContainer'>
                     <div className='formCol'>
                         <label>
                             Recipe Name:
-                            <input type="text" />
+                            <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            />
                         </label>
                         <label>
                             Image:
-                            <input type="text" />
+                            <input
+                            type="text"
+                            value={image}
+                            onChange={(e) => setImage(e.target.value)}
+                            />
                         </label>
                         <label>
                             Cook Time:
-                            <input type="text" />
+                            <input
+                            type="text"
+                            value={cookTime}
+                            onChange={(e) => setCookTime(e.target.value)}
+                            />
                         </label>
                         <label>
                             Serves:
-                            <input type="text" />
+                            <input
+                            type="text"
+                            value={serves}
+                            onChange={(e) => setServes(e.target.value)}
+                            />
                         </label>
-                        <label className='textAreaLabel'>
+                        <label className='textAreaLabel'> {/*There's an error coming from both texteareas, not sure of a way around*/}
                             Description:
-                            <textarea></textarea>
+                            <textarea
+                            onChange={(e) => setDescription(e.target.value)}>
+                                {description}
+                            </textarea>
                         </label>
                         <label className='textAreaLabel'>
                             Preparation:
-                            <textarea></textarea>
+                            <textarea
+                            onChange={(e) => setPreparation(e.target.value)}>
+                                {preparation}
+                            </textarea>
                         </label>
-                        <input type="submit" value="Add Recipe" className='recipeFormSubmit' />
+                        <input
+                        type="submit" value="Add Recipe" className='recipeFormSubmit' />
                     </div>
                     <div className='formCol'>
                         <label className='ingredientLabel'>
                             Ingredients:
                         </label>
-                        {/*Change this to be dependent on ingredient num,
-                        don't know how to format until we have a recipe model*/}
+                        {/*Not really sure how to handle these, The idea is to be able to add fields
+                        and then push them all to an array which gets sent to the backend*/}
                         <input type="text" className='ingredientInput' />
                         <input type="text" className='ingredientInput' />
                         <input type="text" className='ingredientInput' />
