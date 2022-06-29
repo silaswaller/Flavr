01240e8f145d34029b9fff998268802ed35f88dc
Silas Waller <silaswaller1@gmail.com>
2022-06-29 12:37:01 -0400

    Home component almost done, OneRecipe component created

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header';
 import './Home.css';
 import axios from 'axios';
@@ -7,7 +8,7 @@ import axios from 'axios';
 function Home(props) {
 
     const [user, setUser] = useState({});
-
+    const [recipeList, setRecipeList] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/loggedInUser",
@@ -22,13 +23,48 @@ function Home(props) {
             })
     }, [])
 
+    useEffect(() => {
+
+        axios.get("http://localhost:8000/api/recipes")
+        .then((res) => {
+            console.log(res);
+            console.log(res.data);
+            setRecipeList(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, [])
+
     return(
         <div className='homeContainer'>
             <Header />
 
             <div>
-                <h1>Welcome {user.username}</h1>
+                <h1 id="welcomeUsername">Welcome, {user.username}!</h1>
             </div>
+            <div id='container'>
+                
+                {
+                    
+                    recipeList.map((recipe, index) => (
+                        <div>
+                            <div id="homeRecipe" key={recipe._id}>
+                                <Link to={`/Recipe/${recipe._id}`} style={{ color: 'white' }}><h1 id="recipeName">{recipe.name}</h1></Link>
+                                <img className="homeRecipeImage" src={recipe.image} /> 
+                                <div id="likeAndFavorite">
+                                    <div id="like">
+                                        <h4># Likes</h4>
+                                    </div>
+                                    <div id="favorite">
+                                        <h4>Add to Favorites</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+                </div>
         </div>
     );
 };
